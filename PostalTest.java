9f72f797c73364d48c6127389d14ad81c0f54d2e
Sabrina <sabrina.hq@gmail.com>
2016-07-13 11:56:55 +0600

    Second commit

--- PostalTest.java
+++ PostalTest.java
@@ -1,3 +1,4 @@
+//**modified**
 // This file contains material supporting section 2.8 of the textbook:
 // "Object Oriented Software Engineering" and is issued under the open-source
 // license found at www.lloseng.com 
